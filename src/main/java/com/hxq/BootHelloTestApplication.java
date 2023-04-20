4aa68fa6f7fe43a9b85074e4ddb8c5d8582ddcc2
hxq <1483608301@qq.com>
2023-04-20 15:39:39 +0800

    git-test second commit

--- src/main/java/com/hxq/BootHelloTestApplication.java
+++ src/main/java/com/hxq/BootHelloTestApplication.java
@@ -8,6 +8,7 @@ public class BootHelloTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootHelloTestApplication.class, args);
+        System.out.println("git-test");
     }
 
 }
